5dc2915a39a7a6672b2323b25a3a67aee52a6fa1
S. Hwang <sooyeon@gmail.com>
2015-08-09 20:19:13 -0400

    For Google App Engine

---
+++ bot_gae.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.abspath('.'), 'venv/Lib/site-packages'))
+
+import telegram
+from flask import Flask, request
+
+app = Flask(__name__)
+
+global bot
+bot = telegram.Bot(token='TOKEN')
+
+@app.route('/HOOK', methods=['POST'])
+def webhook_handler():
+    if request.method == "POST":
+        # retrieve the message in JSON and then transform it to Telegram object
+        update = telegram.Update.de_json(request.get_json(force=True))
+
+        chat_id = update.message.chat.id
+        text = update.message.text
+
+        # repeat the same message back (echo)
+        bot.sendMessage(chat_id=chat_id, text=text)
+
+    return 'ok'
+
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('https://URL/HOOK')
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+
+@app.route('/')
+def index():
+    return '.'
